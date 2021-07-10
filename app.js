78b135158a78b220a87017509d1e35e50fb01e21
Nandaluri <52715127+Nandaluri@users.noreply.github.com>
2021-07-10 20:57:37 +0200

    redo ads

---
+++ app.js
@@ -0,0 +1,6 @@
+const urlButton = document.getElementById("url-button")
+const urlInput = document.getElementById("url-input")
+
+urlButton.addEventListener("click", () => {
+    console.log(urlInput.value)
+})
